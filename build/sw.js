02ec8db52a6baa6c6d64734cf79a644336b900c1
Gabriel Wicke <gwicke@wikimedia.org>
2016-10-03 16:37:31 -0700

    A bit more cleanup

--- build/sw.js
+++ build/sw.js
@@ -51,8 +51,6 @@
 	const HTMLTransformReader = __webpack_require__(16).HTMLTransformReader;
 	const streamUtil = __webpack_require__(19);
 
-	swt.precache([tplURL]);
-	// swt.options.debug = true;
 	const testHosts = /^https?:\/\/(?:localhost:8934|swproxy.wmflabs.org)\//;
 	function rewriteRequestHost(request) {
 	    if (testHosts.test(request.url)) {
@@ -63,58 +61,45 @@
 	    return request;
 	}
 
-
-	swt.router.default = function(request, options) {
+	// Basic ServiceWorker toolbox setup.
+	swt.precache([tplURL]);
+	// swt.options.debug = true;
+	swt.router.default = (request, options) => {
 	    return swt.networkFirst(rewriteRequestHost(request), options);
 	};
 	swt.options.cache.name = 'default';
 	swt.options.cache.maxEntries = 5000;
 
-	let compiledTemplate;
-	const pageTemplateTransforms = {
-	    transforms: [
-	        {
-	            selector: 'h1[id="firstHeading"]',
-	            handler: (node) => function(ctx) {
-	                return `<h1 id="firstHeading">${ctx.htmlTitle}</h1>`;
-	            }
-	        }, {
-	            selector: 'div[id="mw-content-text"]',
-	            handler: (node) => function(ctx) {
-	                // Use the evaluation context
-	                return fetchBody(ctx.req, ctx.title);
-	            }
-	        }
-	    ]
-	};
-
+	// HTMLTransformReader configuration that matches the body element & returns
+	// innerHTML as a stream.
 	const bodyExtractTransform = {
 	    transforms: [{
-	        selector: {
-	            nodeName: 'body',
-	        },
-	        // Simply pass through the stream for now.
-	        // Later, we can filter the body stream as well.
-	        handler: node => node.innerHTML,
+	        selector: 'body',
+	        // Simply pass through the stream for now. Later, we can add
+	        // transforms on the body stream as well.
+	        handler(node) { return node.innerHTML; },
+	        // Handler wants innerHTML / outerHTML as streams / Reader instances.
 	        stream: true,
 	    }],
+	    // Only return matches & drop un-matched content.
 	    matchOnly: true
 	};
 
 	function fetchBody(req, title) {
-		//const protoHost = req.url.match(/^(https?:\/\/[^\/]+)\//)[1];
-	    return swt.networkFirst(new Request('https://en.wikipedia.org/api/rest_v1/page/html/'
-	                + encodeURIComponent(decodeURIComponent(title))), {
-	                    cache: {
-	                        name: 'api_html',
-	                        maxEntries: 100,
-	                        maxAgeSeconds: 186400
-	                    }
+	    // const protoHost = req.url.match(/^(https?:\/\/[^\/]+)\//)[1];
+	    const normalizedTitle = encodeURIComponent(decodeURIComponent(title));
+	    const url = `https://en.wikipedia.org/api/rest_v1/page/html/${normalizedTitle}`;
+	    return swt.networkFirst(new Request(url), {
+	        cache: {
+	            name: 'api_html',
+	            maxEntries: 100,
+	            maxAgeSeconds: 186400
+	        }
 	    })
-	    .then(res => {
+	    .then((res) => {
 	        if (res.status === 200) {
 	            // Extract body. This reader returns a ReadableStream for the body in its first value.
-	            let reader = new streamUtil.TextDecodeReader(res.body);
+	            const reader = new streamUtil.TextDecodeReader(res.body);
 	            return new HTMLTransformReader(reader, bodyExtractTransform);
 	        } else {
 	            return `<body><em style="color: red">Error fetching body for ${title}:
@@ -134,6 +119,8 @@
 	                'modules=$1%7Cmediawiki.skinning.content.parsoid%7Cext.cite.style&'));
 	}
 
+	// HACK: Replace "Test" (template title) with the current page title in
+	// template source.
 	class HackyReplaceReader {
 	    constructor(input, ctx) {
 	        this._reader = streamUtil.toReader(input);
@@ -142,11 +129,13 @@
 
 	    read() {
 	        return this._reader.read()
-	        .then(res => {
+	        .then((res) => {
 	            if (res.done) {
 	                return res;
 	            }
 	            if (typeof res.value === 'string') {
+	                // Don't care about chunk boundaries for this hack.. but if we
+	                // did, we'd just hold onto the last three bytes.
 	                res.value = res.value.replace(/Test/g, this._ctx.htmlTitle);
 	            }
 	            return res;
@@ -158,12 +147,36 @@
 	    }
 	}
 
+	// Template handlers.
+	const pageTemplateTransforms = {
+	    transforms: [
+	        // Very incomplete.
+	        {
+	            selector: 'h1[id="firstHeading"]',
+	            handler(node) {
+	                return (ctx) => {
+	                    return `<h1 id="firstHeading">${ctx.htmlTitle}</h1>`;
+	                };
+	            }
+	        }, {
+	            selector: 'div[id="mw-content-text"]',
+	            handler(node) {
+	                return (ctx) => {
+	                    // Use the evaluation context
+	                    return fetchBody(ctx.req, ctx.title);
+	                };
+	            }
+	        }
+	    ]
+	};
+
+	let compiledTemplate;
 	function getCompiledTemplate() {
 	    if (compiledTemplate) {
 	        return Promise.resolve(compiledTemplate);
 	    } else {
 	        return getTemplate()
-	            .then(tpl => {
+	            .then((tpl) => {
 	                compiledTemplate = new HTMLTransformReader(tpl, pageTemplateTransforms).drainSync();
 	                return compiledTemplate;
 	            });
@@ -177,10 +190,10 @@
 	};
 
 	function assemblePage(req, tpl) {
-	    var title = req.url.match(/\/w\/?iki\/([^?]+)$/)[1];
+	    const title = req.url.match(/\/w\/?iki\/([^?]+)$/)[1];
 	    const ctx = {
-	        req: req,
-	        title: title,
+	        req,
+	        title,
 	        htmlTitle: title.split('/').map(decodeURIComponent).join('/')
 	                         .replace(/[<"']/g, s => escapes[s])
 	                         .replace(/_/g, ' ')
@@ -194,28 +207,28 @@
 	    reader = new streamUtil.FlatStreamReader(reader, ctx);
 	    // Convert to bytes.
 	    reader = new streamUtil.TextEncodeReader(reader);
-	    // Finally, wrap into a ReadableStream.
+	    // Finally, wrap into a ReadableStream for browser /
+	    // node-serviceworker-proxy consumption.
 	    return streamUtil.toStream(reader);
 	}
 
-	swt.router.get(/https?:\/\/[^\/]+\/w\/?iki\/[^?]+$/,
-	        (request, options) => {
-	            return getCompiledTemplate()
-	            .then((tpl) => {
-	                const body = assemblePage(request, tpl);
-	                return new Response(body, {
-	                    headers: {
-	                        'content-type': 'text/html;charset=utf8'
-	                    }
-	                });
+	// Wiki page entry point
+	swt.router.get(/https?:\/\/[^\/]+\/wiki\/.+$/, (request, options) => {
+	    return getCompiledTemplate()
+	        .then((tpl) => {
+	            const body = assemblePage(request, tpl);
+	            return new Response(body, {
+	                headers: {
+	                    'content-type': 'text/html;charset=utf8'
+	                }
 	            });
 	        });
+	});
 
 	// Use cacheFirst for RL requests
-	swt.router.get("/w/load.php",
-	         (request, options) => {
-	             return swt.cacheFirst(rewriteRequestHost(request), options);
-	         });
+	swt.router.get("/w/load.php", (request, options) => {
+	    return swt.cacheFirst(rewriteRequestHost(request), options);
+	});
 
 	// Boilerplate to ensure our service worker takes control of the page as soon
 	// as possible.
